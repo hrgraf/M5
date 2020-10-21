e623d4c542eaa0f6033b0fd30b3f191233a126f7
H.R.Graf <hrgraf@gmx.ch>
2020-10-21 13:05:52 +0200

    reaching easily 99 updates/s with nunchuck stick or accel

--- Wiimote/Wiimote.ino
+++ Wiimote/Wiimote.ino
@@ -2,55 +2,79 @@
 
 ESP32Wiimote wiimote;
 
+static bool logging = true;
+static long last_ms = 0;
+static int num_run = 0, num_updates = 0;
+
 void setup()
 {
     Serial.begin(115200);
     Serial.println("ESP32Wiimote");
     
     wiimote.init();
-//  wiimote.addFilter(ACTION_IGNORE, FILTER_NUNCHUK_ACCEL);
+    if (! logging)
+        wiimote.addFilter(ACTION_IGNORE, FILTER_NUNCHUK_ACCEL); // optional
     
     Serial.println("Started");
+    last_ms = millis();
 }
 
 void loop()
 {
-  wiimote.task();
-  if (wiimote.available() > 0) 
-  {
-      ButtonState  button  = wiimote.getButtonState();
-      NunchukState nunchuk = wiimote.getNunchukState();
+    wiimote.task();
+    num_run++;
 
-      char ca     = (button & BUTTON_A)     ? 'A' : '.';
-      char cb     = (button & BUTTON_B)     ? 'B' : '.';
-      char cc     = (button & BUTTON_C)     ? 'C' : '.';
-      char cz     = (button & BUTTON_Z)     ? 'Z' : '.';
-      char c1     = (button & BUTTON_ONE)   ? '1' : '.';
-      char c2     = (button & BUTTON_TWO)   ? '2' : '.';
-      char cminus = (button & BUTTON_MINUS) ? '-' : '.';
-      char cplus  = (button & BUTTON_PLUS)  ? '+' : '.';
-      char chome  = (button & BUTTON_HOME)  ? 'H' : '.';
-      char cleft  = (button & BUTTON_LEFT)  ? '<' : '.';
-      char cright = (button & BUTTON_RIGHT) ? '>' : '.';
-      char cup    = (button & BUTTON_UP)    ? '^' : '.';
-      char cdown  = (button & BUTTON_DOWN)  ? 'v' : '.';
+    if (wiimote.available() > 0) 
+    {
+        ButtonState  button  = wiimote.getButtonState();
+        NunchukState nunchuk = wiimote.getNunchukState();
 
-      Serial.printf("button: %05x = ", (int)button);
-      Serial.print(ca);
-      Serial.print(cb);
-      Serial.print(cc);
-      Serial.print(cz);
-      Serial.print(c1);
-      Serial.print(c2);
-      Serial.print(cminus);
-      Serial.print(chome);
-      Serial.print(cplus);
-      Serial.print(cleft);
-      Serial.print(cright);
-      Serial.print(cup);
-      Serial.print(cdown);
-      Serial.printf(", nunchuck.stick: %3d/%3d", nunchuk.xStick, nunchuk.yStick);
-      Serial.printf(", nunchuck.axis: %3d/%3d/%3d\n", nunchuk.xAxis, nunchuk.yAxis, nunchuk.zAxis);
-  }
-  delay(10);
+        num_updates++;
+        if (logging)
+        {
+            char ca     = (button & BUTTON_A)     ? 'A' : '.';
+            char cb     = (button & BUTTON_B)     ? 'B' : '.';
+            char cc     = (button & BUTTON_C)     ? 'C' : '.';
+            char cz     = (button & BUTTON_Z)     ? 'Z' : '.';
+            char c1     = (button & BUTTON_ONE)   ? '1' : '.';
+            char c2     = (button & BUTTON_TWO)   ? '2' : '.';
+            char cminus = (button & BUTTON_MINUS) ? '-' : '.';
+            char cplus  = (button & BUTTON_PLUS)  ? '+' : '.';
+            char chome  = (button & BUTTON_HOME)  ? 'H' : '.';
+            char cleft  = (button & BUTTON_LEFT)  ? '<' : '.';
+            char cright = (button & BUTTON_RIGHT) ? '>' : '.';
+            char cup    = (button & BUTTON_UP)    ? '^' : '.';
+            char cdown  = (button & BUTTON_DOWN)  ? 'v' : '.';
+      
+            Serial.printf("button: %05x = ", (int)button);
+            Serial.print(ca);
+            Serial.print(cb);
+            Serial.print(cc);
+            Serial.print(cz);
+            Serial.print(c1);
+            Serial.print(c2);
+            Serial.print(cminus);
+            Serial.print(chome);
+            Serial.print(cplus);
+            Serial.print(cleft);
+            Serial.print(cright);
+            Serial.print(cup);
+            Serial.print(cdown);
+            Serial.printf(", nunchuck.stick: %3d/%3d", nunchuk.xStick, nunchuk.yStick);
+            Serial.printf(", nunchuck.axis: %3d/%3d/%3d\n", nunchuk.xAxis, nunchuk.yAxis, nunchuk.zAxis);
+        }
+    }
+
+    if (! logging)
+    {
+        long ms = millis();
+        if (ms - last_ms >= 1000)
+        {
+            Serial.printf("Run %d times per second with %d updates\n", num_run, num_updates);
+            num_run = num_updates = 0;
+            last_ms += 1000;
+        }
+    }
+
+    delay(10);
 }
