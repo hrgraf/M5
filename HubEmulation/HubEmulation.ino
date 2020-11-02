82e4f3d50cba06bb25e81e0e26209e5b6b692cdb
H.R.Graf <hrgraf@gmx.ch>
2020-11-02 01:13:24 +0100

    reworked HubEmulation for Boost Hub emulation using Atom Matrix

--- HubEmulation/HubEmulation.ino
+++ HubEmulation/HubEmulation.ino
@@ -1,80 +1,132 @@
 /**
- * A Legoino example to emulate a train hub which directly sends
- * out the motor commands as IR signals to a power function IR receiver. 
- * With this setup you can upgrade your power function systems to powerup systems
+ * A Legoino example to emulate a Boost Move Hub with an Atom Matrix ESP32 device.
  * 
- * For the setup the IR LED has to be connected on the OUTPUT PIN 12 of the 
- * ESP controller. This will work out of the Box with the M5 Atom matrix/light
- * esp32 board which has a build in IR LED on Port 12
+ * (c) Copyright 2020 - Hans-Rudolf Graf
  * 
- * Port A of the powered up hub is mapped to the RED power function port
- * Port B of the powered up hub is mapped to the BLUE power function port
- * 
- * Example video: https://www.youtube.com/watch?v=RTNexxT4-yQ&t=16s
- * 
- * (c) Copyright 2020 - Cornelius Munz
  * Released under MIT License
  * 
  */
 
+#include "M5Atom.h"
 #include "Lpf2HubEmulation.h"
-#include "PowerFunctions.h"
 #include "LegoinoCommon.h"
 
-// create a hub instance
-Lpf2HubEmulation myEmulatedHub("TrainHub", HubType::POWERED_UP_HUB);
+// Emulated hub instance
+Lpf2HubEmulation hub("ESP32 Move Hub", HubType::POWERED_UP_HUB); // BOOST_MOVE_HUB);
+static const byte portA    = (byte)MoveHubPort::A;
+static const byte portB    = (byte)MoveHubPort::B;
+static const byte portC    = (byte)MoveHubPort::C;
+static const byte portD    = (byte)MoveHubPort::D;
+static const byte portTilt = (byte)MoveHubPort::TILT;
 
-// create a power functions instance (IR LED on Pin 12, IR Channel 0)
-PowerFunctions pf(12, 0);
-
+// lego colors
+static CRGB lego_rgb[NUM_COLOR] =
+{
+  CRGB::Black,
+  CRGB::Pink,
+  CRGB::Purple,
+  CRGB::Blue,
+  CRGB::LightBlue,
+  CRGB::Cyan,
+  CRGB::Green,
+  CRGB::Yellow,
+  CRGB::Orange,
+  CRGB::Red,
+  CRGB::White
+};
 
 void writeValueCallback(byte port, byte value)
 {
-  Serial.println("writeValueCallback: ");
-  Serial.println(port, HEX);
-  Serial.println(value, HEX);
+    int svalue = ((int8_t)value);
+    CRGB crgb = CRGB::Black;
+    
+    switch ((MoveHubPort)port)
+    {
+    case MoveHubPort::A:
+        Serial.print("Drive cmd for Port A: ");
+        Serial.println(svalue);
+        break;
 
-  if (port == 0x00)
-  {
-    pf.single_pwm(PowerFunctionsPort::RED, pf.speedToPwm(value));    
-  }
+    case MoveHubPort::B:
+        Serial.print("Drive cmd for Port B: ");
+        Serial.println(svalue);
+        break;
 
-  if (port == 0x01)
-  {
-    pf.single_pwm(PowerFunctionsPort::BLUE, pf.speedToPwm(value));
-  }
+    case MoveHubPort::AB:
+        Serial.print("Drive cmd for Port AB: ");
+        Serial.println(svalue);
+        break;
 
-  if (port == 0x32)
-  {
-    Serial.print("Hub LED command received with color: ");
-    Serial.println(COLOR_STRING[value]);
-  }
+    case MoveHubPort::C:
+        Serial.print("Drive cmd for Port C: ");
+        Serial.println(svalue);
+        break;
+
+    case MoveHubPort::D:
+        Serial.print("Drive cmd for Port D: ");
+        Serial.println(svalue);
+        break;
+
+    case MoveHubPort::LED:
+        Serial.print("LED cmd for color: ");
+        if (value < NUM_COLOR)
+        {
+            Serial.println(COLOR_STRING[value]);
+            crgb = lego_rgb[value];
+        }
+        else
+            Serial.println(value);
+        M5.dis.drawpix(2, 2, crgb);
+        break;
+  
+    default:
+        Serial.print("Unhandled cmd for Port: 0x");
+        Serial.print(port, HEX);
+        Serial.print(", value: 0x");
+        Serial.println(value, HEX);
+    }
 }
 
+// initialization
 void setup()
 {
-  Serial.begin(115200);
+  M5.begin(true, true, true);
+  M5.IMU.Init();
+  M5.dis.fillpix(CRGB::Blue);
+
   // define the callback function if a write message event on the characteristic occurs
-  myEmulatedHub.setWritePortCallback(&writeValueCallback); 
-  myEmulatedHub.start();
+  hub.setWritePortCallback(&writeValueCallback); 
+  hub.start();
+  Serial.println("Started ESP32 Move Hub...");
 }
 
 // main loop
 void loop()
 {
-
   // if an app is connected, attach some devices on the ports to signalize 
   // the app that values could be received/written to that ports
-  if (myEmulatedHub.isConnected && !myEmulatedHub.isPortInitialized)
+  if (hub.isConnected && !hub.isPortInitialized)
   {
+    Serial.println("Connected");
+    M5.dis.fillpix(CRGB::Black);
+
     delay(1000);
-    myEmulatedHub.isPortInitialized = true;
-    myEmulatedHub.attachDevice((byte)PoweredUpHubPort::A, DeviceType::TRAIN_MOTOR);
+    hub.isPortInitialized = true;
+    hub.attachDevice((byte)MoveHubPort::A, DeviceType::MOVE_HUB_MEDIUM_LINEAR_MOTOR);
     delay(1000);
-    myEmulatedHub.attachDevice((byte)PoweredUpHubPort::LED, DeviceType::HUB_LED);
+    hub.attachDevice((byte)MoveHubPort::LED, DeviceType::HUB_LED);
     delay(1000);
-    myEmulatedHub.attachDevice((byte)PoweredUpHubPort::B, DeviceType::TRAIN_MOTOR);
+    hub.attachDevice((byte)MoveHubPort::B, DeviceType::MOVE_HUB_MEDIUM_LINEAR_MOTOR);
     delay(1000);
+
+    M5.dis.fillpix(CRGB::Green);
+  }
+
+  if (!hub.isConnected && hub.isPortInitialized)
+  {
+    Serial.println("Disconnected");
+    hub.isPortInitialized = false;
+    M5.dis.fillpix(CRGB::Red);
   }
 
 } // End of loop
